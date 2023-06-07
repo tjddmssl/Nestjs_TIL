6b145fc2f8d020454518dcffdfee711cdc687a2f
tjddmssl <tjddmssl910103@gmail.com>
2023-06-08 03:54:48 +0900

    commit

---
+++ next route/src/app/products/[slug]/page.tsx
@@ -0,0 +1,27 @@
+import { notFound } from 'next/navigation';
+
+type Props = {
+  params: {
+    slug: string;
+  };
+};
+
+export function generateMetadata({ params }: Props) {
+  return {
+    title: `제품의 이름: ${params.slug}`,
+  };
+}
+
+export default function PantsPage({ params }: Props) {
+  if (params.slug === 'nothing') {
+    notFound();
+  }
+  return <h1>{params.slug} 제품 설명 페이지</h1>;
+}
+
+export function generateStaticParams() {
+  const products = ['pants', 'skirt'];
+  return products.map((product) => ({
+    slug: product,
+  }));
+}
